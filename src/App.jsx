0cbb69396640cdbbf2ce9e38cdcea0a5b7ffddb5
teampenajaya <teampenaslot@gmail.com>
2025-03-03 03:51:25 +0000

    update frontend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DirectComplaintForm from "./DirectComplaintForm";
+
+function App() {
+  return (
+    <>
+      <DirectComplaintForm />
+    </>
+  );
+}
+
+export default App;
